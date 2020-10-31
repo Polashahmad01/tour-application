c723a56481c29d2662efabab5438a0d69672264b
Polashahmad01 <dev.polashahmad@gmai.com>
2020-10-31 14:06:25 +0600

    full features complete

---
+++ src/components/Tours.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = ({ tours, removeTour }) => {
+
+    const renderTourList = (tours) =>  {
+        return tours.map((tour) => {
+            return <Tour key={tour.id} { ...tour} removeTour={removeTour} />
+        });
+    }
+
+    return (
+        <section>
+            <div className="title">
+                <h2>Our Tours</h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {renderTourList(tours)}
+            </div>
+        </section>
+    );
+};
+
+export default Tours;
